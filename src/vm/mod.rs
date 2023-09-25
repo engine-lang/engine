bfe67767b3ed4aed3b47b20bbd41bad4536c76ea
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2023-09-25 08:07:07 +0300

    Add Engine VM (#3)

---
+++ src/vm/mod.rs
@@ -0,0 +1,103 @@
+mod tokens;
+mod file;
+mod syntax_tree;
+mod environments;
+mod instructions_construct;
+mod assign_instructions;
+mod convert_instructions;
+mod operation_instructions;
+mod instructions_executers;
+
+use std::collections::HashMap;
+use std::panic;
+use std::env;
+
+use environments::{
+    Environment,
+    EnvironmentScope
+};
+use syntax_tree::InstructionType;
+use file::File;
+use instructions_construct::construct_instruction;
+use instructions_executers::execute_instruction;
+
+
+pub fn execute_byte_code() -> Result<(), String>{
+    let args: Vec<String> = env::args().collect();
+    panic::set_hook(Box::new(|panic_info| {
+        if let Some(panic_message) = panic_info.payload().downcast_ref::<String>() {
+            println!("{}", panic_message);
+        } else if let Some(panic_message) = panic_info.payload().downcast_ref::<&str>() {
+            println!("{}", panic_message);
+        } else {
+            println!("EngineVM -> VM Error");
+        }
+    }));
+
+    let mut environment = Environment {
+        scope: EnvironmentScope::Main,
+        variables: HashMap::new()
+    };
+
+    let mut file = File::open(&args[1]);
+
+    let info = file.read_line();
+    if info.is_err(){
+        panic!(
+            "Engine VM: File Error -> {}, line: 1:1",
+            info.unwrap_err());
+    }
+
+    let mut current_line: u128 = 1;
+
+    let mut goto_lines_stack: Vec<u128> = Vec::new();
+
+    loop{
+        let line = file.read_line();
+        if line.is_err(){
+            panic!(
+                "Engine VM: File Error -> {}, line: {}:1",
+                line.unwrap_err(), current_line);
+        }
+        current_line += 1;
+
+        if line.as_ref().unwrap().1 == 0{
+            break;
+        }
+
+        let instruction = construct_instruction(line.unwrap().0, current_line);
+        if instruction.is_err(){
+            panic!("{}", instruction.unwrap_err());
+        }
+        let instruction = instruction.unwrap();
+
+        let instruction_line = instruction.1;
+        let instruction = instruction.0;
+
+        if instruction.instruction_type == Some(InstructionType::End) ||
+            instruction.instruction_type == None
+        {
+            break;
+        }
+
+        if goto_lines_stack.len() > 0{
+            if &&instruction_line != goto_lines_stack.last().as_ref().unwrap(){
+                continue;
+            }
+            goto_lines_stack.pop();
+        }
+
+        let result = execute_instruction(
+            current_line, &mut environment, instruction);
+        if result.is_err(){
+            panic!("{}", result.unwrap_err());
+        }
+        let result = result.unwrap();
+
+        if !result.0{
+            goto_lines_stack.push(result.1);
+        }
+    }
+
+    return Ok(());
+}
---
+++ src/vm/syntax_tree.rs
@@ -0,0 +1,96 @@
+use crate::vm::tokens::TokenType;
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum InstructionType {
+    Assign,
+    Convert,
+    Operation,
+
+    Print,
+    Input,
+
+    If,
+    Else,
+    GoTo,
+
+    End
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct AssignInstruction{
+    pub assign_type: Option<TokenType>,
+    pub variable_name: Option<String>,
+    pub value: Option<String>,
+    pub line: Option<u128>,
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ConvertInstruction{
+    pub convertion_type: Option<TokenType>,
+    pub convert_to: Option<String>,
+    pub convert_from: Option<String>,
+    pub line: Option<u128>,
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct OperationInstruction{
+    pub operation_type: Option<TokenType>,
+    pub left_variable: Option<String>,
+    pub right_variable: Option<String>,
+    pub assign_to_variable: Option<String>,
+    pub line: Option<u128>,
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct PrintInstruction{
+    pub variable_name: Option<String>,
+    pub line: Option<u128>,
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct InputInstruction{
+    pub variable_name: Option<String>,
+    pub line: Option<u128>,
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct IfInstruction{
+    pub variable_name: Option<String>,
+    pub condition_fail_goto_line: Option<u128>,
+    pub line: Option<u128>,
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ElseInstruction{
+    pub line: Option<u128>,
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct GoToInstruction{
+    pub goto_line: Option<u128>,
+    pub line: Option<u128>,
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Instruction{
+    pub instruction_type: Option<InstructionType>,
+    pub assign_instruction: Option<AssignInstruction>,
+    pub convert_instruction: Option<ConvertInstruction>,
+    pub operation_instruction: Option<OperationInstruction>,
+    pub print_instruction: Option<PrintInstruction>,
+    pub input_instruction: Option<InputInstruction>,
+
+    pub if_instruction: Option<IfInstruction>,
+    pub else_instruction: Option<ElseInstruction>,
+    pub goto_instruction: Option<GoToInstruction>,
+}
